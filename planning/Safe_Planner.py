f4abc92a8e214979a85bd5dc3be47a86a1c8d02e
May0Mei <zm2074@princeton.edu>
2024-01-24 13:45:41 -0500

    fix bugs

--- planning/Safe_Planner.py
+++ planning/Safe_Planner.py
@@ -114,7 +114,7 @@ class World:
                 xs, ys = self.free_space_new.exterior.xy
                 ax.fill(xs,ys, edgecolor = 'k', linestyle='--', fc = (0,0,0,0))
             else:
-                for geom in self.free_space.geoms:
+                for geom in self.free_space_new.geoms:
                     if geom.geom_type == 'Polygon':
                         xs, ys = geom.exterior.xy
                         ax.fill(xs,ys, edgecolor = 'k', linestyle='--', fc = (0,0,0,0))
