9239a3ce2bc893b7231a6bda6b3f5142350b9e85
Anushri Dixit <anushridixit111@gmail.com>
2024-03-26 10:49:58 -0400

    Merged task env from sim branch + new requirements

--- nav_sim/env/task_env.py
+++ nav_sim/env/task_env.py
@@ -10,6 +10,8 @@ import pybullet as pb
 from pybullet_utils import bullet_client as bc
 
 from nav_sim.env.vanilla_env import VanillaEnv
+from robot_descriptions.loaders.pybullet import load_robot_description
+
 
 
 class TaskEnv(VanillaEnv):
@@ -44,23 +46,23 @@ class TaskEnv(VanillaEnv):
             p.resetDebugVisualizerCamera(3.80, 225, -60, [3.5, 0.5, 0])
 
         # Build camera and LiDAR visualization if render
-        if self.render:
-            camera_visual_id = p.createVisualShape(
-                p.GEOM_BOX, rgbaColor=[0, 0.8, 0, 1.0],
-                halfExtents=[0.03, 0.20, 0.05]
-            )
-            self.camera_id = p.createMultiBody(
-                baseMass=0, baseCollisionShapeIndex=-1,
-                baseVisualShapeIndex=camera_visual_id, basePosition=[0, 0, 0]
-            )
-            lidar_visual_id = p.createVisualShape(
-                p.GEOM_CYLINDER, rgbaColor=[0, 0.8, 0, 1.0], radius=0.08,
-                length=self.lidar_height
-            )
-            self.lidar_id = p.createMultiBody(
-                baseMass=0, baseCollisionShapeIndex=-1,
-                baseVisualShapeIndex=lidar_visual_id, basePosition=[0, 0, 0]
-            )
+        # if self.render:
+        #     camera_visual_id = p.createVisualShape(
+        #         p.GEOM_BOX, rgbaColor=[0, 0.8, 0, 1.0],
+        #         halfExtents=[0.03, 0.20, 0.05]
+        #     )
+        #     self.camera_id = p.createMultiBody(
+        #         baseMass=0, baseCollisionShapeIndex=-1,
+        #         baseVisualShapeIndex=camera_visual_id, basePosition=[0, 0, 0]
+        #     )
+        #     lidar_visual_id = p.createVisualShape(
+        #         p.GEOM_CYLINDER, rgbaColor=[0, 0.8, 0, 1.0], radius=0.08,
+        #         length=self.lidar_height
+        #     )
+        #     self.lidar_id = p.createMultiBody(
+        #         baseMass=0, baseCollisionShapeIndex=-1,
+        #         baseVisualShapeIndex=lidar_visual_id, basePosition=[0, 0, 0]
+        #     )
 
         # Initialize obstacle id list - excluding walls and floors
         self._obs_id_all = []
@@ -157,16 +159,17 @@ class TaskEnv(VanillaEnv):
                 ornObj=self._p.getQuaternionFromEuler([0, 0, state[2]]),
             )
         else:
-            robot_visual_id = self._p.createVisualShape(
-                self._p.GEOM_BOX,
-                halfExtents=self.robot_half_dim,
-                rgbaColor=[0.5, 0.5, 0.5, 1],
-            )
-            self.robot_id = self._p.createMultiBody(
-                baseMass=0,  # static
-                baseVisualShapeIndex=robot_visual_id,
-                basePosition=np.append(state[:2], self.robot_com_height),
-                baseOrientation=self._p.getQuaternionFromEuler([
-                    0, 0, state[2]
-                ])
-            )
+            # robot_visual_id = self._p.createVisualShape(
+            #     self._p.GEOM_BOX,
+            #     halfExtents=self.robot_half_dim,
+            #     rgbaColor=[0.5, 0.5, 0.5, 1],
+            # )
+            # self.robot_id = self._p.createMultiBody(
+            #     baseMass=0,  # static
+            #     baseVisualShapeIndex=robot_visual_id,
+            #     basePosition=np.append(state[:2], self.robot_com_height),
+            #     baseOrientation=self._p.getQuaternionFromEuler([
+            #         0, 0, state[2]
+            #     ])
+            # )
+            self.robot_id = load_robot_description('go1_description')
